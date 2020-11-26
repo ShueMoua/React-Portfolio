9eaf429b898dad4ffba93c40f9b08b3a893d2e55
Shuelong Moua <moua_shuelong@yahoo.com>
2020-11-26 00:08:32 -0600

    imported files(AboutMe, Portfolio, NavBar, and dependencies)

--- newreact/src/App.js
+++ newreact/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import AboutMe from "./pages/AboutMe";
+import Portfolio from "./pages/Portfolio";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <NavBar />
+                <Route exact path="/" component={AboutMe} />
+                <Route exact path="/Portfolio" component={Portfolio} />
+            </div>
+
+        </Router>
+    )
 }
+
 
 export default App;
