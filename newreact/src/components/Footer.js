2fc8ae60085b7264103c9323a4b20123d9f9fac4
Shuelong Moua <moua_shuelong@yahoo.com>
2020-11-26 01:17:45 -0600

    create footer

---
+++ newreact/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer className="footer bg-info fixed-bottom">
+            <p className="text-white text-center"> Contact Me: &nbsp;&nbsp; Moua_Shuelong@yahoo.com</p>
+        </footer>
+    )
+}
+
+export default Footer;
