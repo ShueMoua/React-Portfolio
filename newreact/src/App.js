77f9085afcea5d68025d507f44c973bc2f4e442f
Shuelong Moua <moua_shuelong@yahoo.com>
2020-12-08 12:18:33 -0600

    attempting to deploy correctly

--- newreact/src/App.js
+++ newreact/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
-import AboutMe from "./pages/AboutMe";
+import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 
 
@@ -9,13 +9,10 @@ function App() {
     return (
         <Router>
             <div>
-                <NavBar />
-            
-                <Route exact path="/" component={AboutMe} />
+                < NavBar />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/Portfolio" component={Portfolio} />
-                
             </div>
-
         </Router>
     )
 }
--- newreact/src/pages/Portfolio.js
+++ newreact/src/pages/Portfolio.js
@@ -8,68 +8,69 @@ import NoteTaker from "../pics/note_taker.JPG";
 import MathQuiz from "../pics/math-quiz.JPG";
 import Footer from "../components/Footer";
 
-const Portfolio = () => (
-  <div className="bg-secondary">
-    <br />
-    <h2 className="text-center text-white">My Projects</h2>
-    <br />
-    <div className="card">
-      <div className="img-container">
-        <img alt="budget tracker" src={budgetTracker} />
+function Portfolio() {
+  return (
+    <div className="bg-secondary">
+      <br />
+      <h2 className="text-center text-white">My Projects</h2>
+      <br />
+      <div className="card">
+        <div className="img-container">
+          <img alt="budget tracker" src={budgetTracker} />
+        </div>
+        <div className="content">
+          <p>This Budget Tracker app will help you keep track of your income and expenses.</p>
+          <a href="https://github.com/ShueMoua/Budget_Tracker"> Budget Tracker</a>
+        </div>
       </div>
-      <div className="content">
-        <p>This Budget Tracker app will help you keep track of your income and expenses.</p>
-        <a href="https://github.com/ShueMoua/Budget_Tracker"> Budget Tracker</a>
+      <div className="card">
+        <div className="img-container">
+          <img alt="easy shopper" src={easyShopper} />
+        </div>
+        <div className="content">
+          <p>The Easy Shopper app will assist you with comparing prices for an item between Target and Amazon.</p>
+          <a href="https://github.com/ShueMoua/Price-Comparison-App">Easy Shopper</a>
+        </div>
       </div>
-    </div>
-    <div className="card">
-      <div className="img-container">
-        <img alt="easy shopper" src={easyShopper} />
+      <div className="card">
+        <div className="img-container">
+          <img alt="password generator" src={passwordGenerator} />
+        </div>
+        <div className="content">
+          <p>If you're having trouble thinking of a password, look no further. The Password Generator app could generate a password for up to 128 characters.</p>
+          <a href="https://github.com/ShueMoua/Password-Generator">Password Generator</a>
+        </div>
       </div>
-      <div className="content">
-        <p>The Easy Shopper app will assist you with comparing prices for an item between Target and Amazon.</p>
-        <a href="https://github.com/ShueMoua/Price-Comparison-App">Easy Shopper</a>
+      <div className="card">
+        <div className="img-container">
+          <img alt="weather dashboard" src={weatherDashboard} />
+        </div>
+        <div className="content">
+          <p>The Weather Dashboard will assist you with getting the weather reports for any city you're in.</p>
+          <a href="https://github.com/ShueMoua/Weather-Dashboard">Weather Dashboard</a>
+        </div>
       </div>
-    </div>
-    <div className="card">
-      <div className="img-container">
-        <img alt="password generator" src={passwordGenerator} />
+      <div className="card">
+        <div className="img-container">
+          <img alt="Note Taker" src={NoteTaker} />
+        </div>
+        <div className="content">
+          <p>This Note Taker app will assist you with keeping track of your daily goals/planning.</p>
+          <a href="https://github.com/ShueMoua/Note-Taker.git">Note Taker</a>
+        </div>
       </div>
-      <div className="content">
-        <p>If you're having trouble thinking of a password, look no further. The Password Generator app could generate a password for up to 128 characters.</p>
-        <a href="https://github.com/ShueMoua/Password-Generator">Password Generator</a>
+      <div className="card">
+        <div className="img-container">
+          <img alt="Math Quiz" src={MathQuiz} />
+        </div>
+        <div className="content">
+          <p>This quiz will test your mathematics skills.</p>
+          <a href="https://github.com/ShueMoua/Math-Quiz.git">Math Quiz</a>
+        </div>
       </div>
-    </div>
-    <div className="card">
-      <div className="img-container">
-        <img alt="weather dashboard" src={weatherDashboard} />
-      </div>
-      <div className="content">
-        <p>The Weather Dashboard will assist you with getting the weather reports for any city you're in.</p>
-        <a href="https://github.com/ShueMoua/Weather-Dashboard">Weather Dashboard</a>
-      </div>
-    </div>
-    <div className="card">
-      <div className="img-container">
-        <img alt="Note Taker" src={NoteTaker} />
-      </div>
-      <div className="content">
-        <p>This Note Taker app will assist you with keeping track of your daily goals/planning.</p>
-        <a href="https://github.com/ShueMoua/Note-Taker.git">Note Taker</a>
-      </div>
-    </div>
-    <div className="card">
-      <div className="img-container">
-        <img alt="Math Quiz" src={MathQuiz} />
-      </div>
-      <div className="content">
-        <p>This quiz will test your mathematics skills.</p>
-        <a href="https://github.com/ShueMoua/Math-Quiz.git">Math Quiz</a>
-      </div>
-    </div>
 
-    <Footer />
-  </div>
-);
-
+      <Footer />
+    </div>
+  );
+}
 export default Portfolio;
